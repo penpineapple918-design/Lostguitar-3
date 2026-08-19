872a5a34ae10c80bd39900b1d18abd17bd514c37
penpineapple918-design <penpineapple918@gmail.com>
2026-08-18 22:30:44 -0700

    Update MainActivity.java

--- app/src/main/MainActivity.java
+++ app/src/main/MainActivity.java
@@ -4,52 +4,73 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
-import android.webkit.*;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.ValueCallback;
+import android.webkit.FileChooserParams;
+
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
 
-    WebView web;
-    ValueCallback<Uri[]> callback;
+    private WebView web;
+    private ValueCallback<Uri[]> chooserCallback;
 
-    static final int MP3 = 42;
-    static final int FILE = 43;
+    private static final int FILE_CHOOSER = 100;
+    private static final int MP3 = 101;
+    private static final int PFP = 102;
+    private static final int BANNER = 103;
 
     @Override
-    public void onCreate(Bundle b) {
-        super.onCreate(b);
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
 
         web = new WebView(this);
 
         web.getSettings().setJavaScriptEnabled(true);
         web.getSettings().setDomStorageEnabled(true);
+        web.getSettings().setAllowFileAccess(true);
 
         web.setWebViewClient(new WebViewClient());
 
         web.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onShowFileChooser(
-                    WebView v,
-                    ValueCallback<Uri[]> c,
-                    FileChooserParams p) {
+                    WebView view,
+                    ValueCallback<Uri[]> callback,
+                    FileChooserParams params) {
 
-                callback = c;
+                chooserCallback = callback;
 
                 Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 i.addCategory(Intent.CATEGORY_OPENABLE);
                 i.setType("*/*");
 
-                startActivityForResult(i, FILE);
+                startActivityForResult(i, FILE_CHOOSER);
                 return true;
             }
         });
 
-        web.addJavascriptInterface(new Bridge(), "Android");
+        web.addJavascriptInterface(new AndroidBridge(), "Android");
+
+        web.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                wireButtons();
+            }
+        });
+
         web.loadUrl("file:///android_asset/index.html");
 
         setContentView(web);
     }
 
-    class Bridge {
+    /* ---------------- ANDROID BRIDGE ---------------- */
+
+    private class AndroidBridge {
 
         @JavascriptInterface
         public void pickMp3() {
@@ -64,7 +85,7 @@ public class MainActivity extends Activity {
             Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             i.addCategory(Intent.CATEGORY_OPENABLE);
             i.setType("image/*");
-            startActivityForResult(i, FILE);
+            startActivityForResult(i, PFP);
         }
 
         @JavascriptInterface
@@ -72,31 +93,124 @@ public class MainActivity extends Activity {
             Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             i.addCategory(Intent.CATEGORY_OPENABLE);
             i.setType("image/*");
-            startActivityForResult(i, FILE);
+            startActivityForResult(i, BANNER);
         }
     }
 
-    @Override
-    protected void onActivityResult(int r, int c, Intent d) {
-        super.onActivityResult(r, c, d);
+    /* ---------------- CONNECT HTML BUTTONS ---------------- */
 
-        if (r == FILE && callback != null) {
-            callback.onReceiveValue(
-                    c == RESULT_OK && d != null
-                            ? new Uri[]{d.getData()}
-                            : null
-            );
-            callback = null;
+    private void wireButtons() {
+
+        String js =
+            "(function(){"
+
+          + "function findButtons(){"
+          + "var b=document.querySelectorAll('button');"
+
+          + "b.forEach(function(x){"
+          + "var t=(x.innerText||'').toLowerCase();"
+
+          + "if(t.includes('change pfp'))"
+          + "x.onclick=function(){Android.pickPFP()};"
+
+          + "if(t.includes('change banner'))"
+          + "x.onclick=function(){Android.pickBanner()};"
+
+          + "if(t.includes('upload song')||t.includes('upload mp3'))"
+          + "x.onclick=function(){Android.pickMp3()};"
+
+          + "});"
+          + "}"
+
+          /* PFP */
+          + "window.setPfp=function(uri){"
+          + "var p=document.querySelector('.pfp');"
+          + "if(p){"
+          + "p.style.backgroundImage='url('+uri+')';"
+          + "p.style.backgroundSize='cover';"
+          + "p.style.backgroundPosition='center';"
+          + "}"
+          + "};"
+
+          /* Banner */
+          + "window.setBanner=function(uri){"
+          + "var b=document.querySelector('.banner');"
+          + "if(b){"
+          + "b.style.backgroundImage='url('+uri+')';"
+          + "b.style.backgroundSize='cover';"
+          + "b.style.backgroundPosition='center';"
+          + "}"
+          + "};"
+
+          /* MP3 */
+          + "window.addImportedMp3=function(uri){"
+          + "window.lostGuitarSong=uri;"
+          + "var title=document.querySelector('.song h2');"
+          + "var sub=document.querySelector('.song p');"
+          + "if(title)title.textContent='Song Ready';"
+          + "if(sub)sub.textContent='MP3 loaded successfully';"
+          + "};"
+
+          + "findButtons();"
+
+          + "})();";
+
+        web.evaluateJavascript(js, null);
+    }
+
+    /* ---------------- PICKER RESULTS ---------------- */
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == FILE_CHOOSER) {
+
+            if (chooserCallback != null) {
+
+                if (resultCode == RESULT_OK && data != null && data.getData() != null) {
+                    chooserCallback.onReceiveValue(
+                            new Uri[]{data.getData()}
+                    );
+                } else {
+                    chooserCallback.onReceiveValue(null);
+                }
+
+                chooserCallback = null;
+            }
+
             return;
         }
 
-        if (r == MP3 && c == RESULT_OK && d != null) {
-            String u = d.getData().toString()
-                    .replace("\\", "\\\\")
-                    .replace("\"", "\\\"");
+        if (resultCode != RESULT_OK || data == null || data.getData() == null) {
+            return;
+        }
 
+        String uri = JSONObject.quote(
+                data.getData().toString()
+        );
+
+        if (requestCode == PFP) {
             web.evaluateJavascript(
-                    "window.addImportedMp3(\"" + u + "\")",
+                    "window.setPfp(" + uri + ");",
+                    null
+            );
+        }
+
+        if (requestCode == BANNER) {
+            web.evaluateJavascript(
+                    "window.setBanner(" + uri + ");",
+                    null
+            );
+        }
+
+        if (requestCode == MP3) {
+            web.evaluateJavascript(
+                    "window.addImportedMp3(" + uri + ");",
                     null
             );
         }
